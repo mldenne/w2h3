4f4ff63e51aaf08b5b01ab931b40137148b97ea8
Marie Denney <mldenne@gmail.com>
2016-06-22 14:06:47 -0400

    First commit

---
+++ robot.rb
@@ -0,0 +1,16 @@
+# Define a Robot Class
+class Robot
+# A robot has a name
+  def name(name)
+    puts name
+  end
+# A robot says "Hi!"
+  def say_hi
+    puts "Hi!"
+  end
+
+# A robot tells  you his name
+  def say_name
+    puts "My name is #{name}"
+  end
+end
