600b735bffa66d18ccad6f1c78e5f970e71795db
Marie Denney <mldenne@gmail.com>
2016-06-22 14:52:40 -0400

    Explorer Mode completed commit

--- robot.rb
+++ robot.rb
@@ -9,8 +9,36 @@ class Robot
     puts "Hi!"
   end
 
-# A robot tells  you his name
-  def say_name
+# A robot tells you his name
+  def say_name(name)
     puts "My name is #{name}"
+  end
+end
+
+# Define a BendingUnit class that inherits from Robot class
+class BendingUnit < Robot
+
+# A BendingUnit instance has a method called 'bend'
+  def bend(objecttobend) # with one argument
+    puts "Bend #{objecttobend}!" # that puts what the object is to bend
+  end
+end
+
+# Define an ActorUnit class that inherits from Robot class
+class ActorUnit < Robot
+
+# An ActorUnit instance has a method called 'change_name'
+# change_name method accepts an argument 'newname'
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def change_name(newname)
+    @name = newname
   end
 end
